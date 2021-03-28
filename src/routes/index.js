0ca6cef1a102af0376e7e51fdb391c1eebde0913
jacksonAquino <jackson.primo12@gmail.com>
2021-03-27 23:04:09 -0300

    increase routes file

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+router.use('/users', require('./Users'))
+
+router.use('/*', function (req, res) {
+  res
+    .status(404)
+    .json({"error": "Rota não encontrada"})
+});
+
+module.exports = router;
