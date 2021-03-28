99ed7837316bd85519b5468e05ddf7959814c400
jacksonAquino <jackson.primo12@gmail.com>
2021-03-27 23:03:28 -0300

    move server to src

--- src/server.js
+++ src/server.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
   res.send('TATAKAE FAMILIA')
 })
 
+app.use(express.json())
+
+
+app.use(require('./routes'))
+
+
 app.listen(PORT, () =>{
   console.log(`Server is running in port ${PORT}`)
 })
