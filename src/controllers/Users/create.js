ef2b22e06224ae193872859d5439b2ef7f68b19d
jacksonAquino <jackson.primo12@gmail.com>
2021-04-08 13:28:40 -0300

    increase create user

--- src/controllers/Users/create.js
+++ src/controllers/Users/create.js
@@ -1,5 +1,26 @@
+const User = require('../../database/models/User')
 module.exports = async (req, resp) => {
   const body = req.body
-  const { email, password } = body;
+  const { email, password, name } = body;
+  //validar valores
+  const createData = {
+    email,
+    password,
+    name,
+    rule: 'USER'
+  }
+  try {
+    const user = await User.create(createData)
+    if(user) resp.json({
+      user: user.dataValues.id,
+      message: 'sucesso ao criar user'
+    })
+  }
+  catch(err){
+    resp.send({
+      err,
+      message: 'erro ao cadastrar usuário'
+    })
+  }
 	resp.json({email, password})
 };
