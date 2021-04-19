46ad59ea8f922ee7534b2c28945ac2096c607ec5
jacksonPrimo <jackson.primo12@gmail.com>
2021-04-19 13:26:43 -0300

    finish session test

--- src/test/integration/api/session.test.js
+++ src/test/integration/api/session.test.js
@@ -3,13 +3,13 @@ require('dotenv-flow').config()
 const app = require('../../../app')
 const sequelize = require('../../../database/models')
 
-const DEFAULT_USER = {
+const newUser = {
   firstName: "John",
   lastName: "Doe",
   password: "testpassword",
   birthDate: "01/01/2021",
   phone: "0000000000",
-  email: "email@test.com",
+  email: "test@test.com",
   type: "user"
 }
 
@@ -17,7 +17,7 @@ const API_SESSION = '/session'
 
 beforeAll(async () => {
   await sequelize.sequelize.sync({ force: true })
-  // await request(app).post('/users').send(DEFAULT_USER);
+  await request(app).post('/users').send(newUser);
 })
 
 afterAll(async () => {
@@ -27,10 +27,26 @@ afterAll(async () => {
 describe("Authentication", ()=>{
   test('should authenticated with valid credential', async()=>{
     const user = {
-      email: "email@test.com",
+      email: "test@test.com",
       password: "testpassword",
     }
     const response = await request(app).post(API_SESSION).send(user)
     expect(response.statusCode).toBe(200)
+  })
+  test('should fail because user with email not found', async()=>{
+    const user = {
+      email: "test@test.com.br",
+      password: "testpassword",
+    }
+    const response = await request(app).post(API_SESSION).send(user)
+    expect(response.statusCode).toBe(404)
+  })
+  test('should fail because credential it is invalid', async()=>{
+    const user = {
+      email: "test@test.com",
+      password: "testpassword123",
+    }
+    const response = await request(app).post(API_SESSION).send(user)
+    expect(response.statusCode).toBe(401)
   })
 })
